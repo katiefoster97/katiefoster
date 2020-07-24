ae2a776270840094ca92175879fc4cab762cce09
katiefoster97 <67602788+katiefoster97@users.noreply.github.com>
2020-07-24 08:54:56 -0700

    commit on master

--- functions.js
+++ functions.js
@@ -5,6 +5,7 @@
   //  alert(HelloWorldofJavascript);
 //}
 
+//practicing my js below
 var x, y;
 x = 5 + 6;
 y = x * 10;
@@ -15,3 +16,33 @@ function sayHi() {
     elemRef.innerHTML = "goodbye";
 
 }
+//adding a function to my index.html to reveal an answer
+
+function myFunctionreveal() {
+    document.getElementById("jsbutton").innerHTML = "Milan, Italy";
+  }
+
+  function myFunctionlook() {
+    document.getElementById("jsbuttonstyle").style.color = "blue";
+  }
+
+  //using js to add a new element to my index.html file
+  //inserting after a <p> tag
+ // was unable to determine how to insert after
+    var para = document.createElement("p");
+    var node = document.createTextNode("Paris, France");
+    para.appendChild(node);
+
+    var element = document.getElementById("jsnew");
+    var child = document.getElementById("p1");
+    element.insertBefore(para,child);
+    element.classList.add("jsstyle");
+
+//using js to return response upon text being submitted in 
+//html element
+
+function myform() {
+    alert("Wow, that is cool!");
+  }
+  
+
