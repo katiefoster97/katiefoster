3b121c8edab4277c0539402ce4013507a950098e
katiefoster97 <67602788+katiefoster97@users.noreply.github.com>
2020-07-30 06:01:29 -0700

    commit on master

--- jsassign.js
+++ jsassign.js
@@ -1,20 +1,25 @@
 // setting up a separate js file for week4 assignment
 
-function jsButtonFunction(){
-    console.log("jsbutton");
-    var input = document.getElementById("user");
-    var num = input.value;
-    console.log(num);
-    getRandomNumber(num);
-  }
-function getRandomNumber(number){
-    number = Math.random() * number;
-    var img = document.createElement("img");
-    img.src = "feet_01.jpg";
-    img.style.backgroundColor = "blue"
+//working on random number generator and still have 1 error to debug
 
-    var src = document.getElementById("p1");
-    src.appendChild(img);
+function randomNum () {
+    var input = document.getElementById("number_select");
+    var number = input.value;
+    var randNum = Math.floor(Math.random() * number);
+  
+    return (randNum);
+  }
+function numberDiv () {
+    randomNum();
+    document.getElementById("numberdiv").innerHTML = randomNum ();
+}
+    //number = Math.random() * number;
+    //var img = document.createElement("img");
+    //img.src = "feet_01.jpg";
+    //img.style.backgroundColor = "blue"
+
+   // var src = document.getElementById("p1");
+    //src.appendChild(img);
 
     //document.body.insertBefore(newDiv, currentDiv);
 
